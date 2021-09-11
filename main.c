dd1dae058ab877f762c1afea2ed4776a4f1f37e4
dogukanteber <dogukanteber1@hotmail.com>
2021-09-11 23:44:41 +0300

    Add functionality of reading from a file.

--- main.c
+++ main.c
@@ -1,33 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define ROW 9
 #define COL 9
+#define BOARD_SIZE (ROW * COL)
 
 #define EMPTY 0
 
+
+int* read_file(const char* file_name) {
+    FILE* file;
+
+    file = fopen(file_name, "r");
+    if ( file == NULL ) {
+        fprintf(stdout, "Could find the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int* digits = malloc(BOARD_SIZE * sizeof(int));
+    size_t pos = 0;
+    int digit;
+
+    // read the file character by character
+    while ( (digit = fgetc(file)) != EOF ) {
+        // if the current char is a digit
+        if ( isdigit(digit) ) {
+            // put its value as integer to digits array
+            digits[pos++] = digit - '0';
+        }
+    }
+    return digits;
+}
+
+
+void init_board(int board[][COL], const char* file_name) {
+    int* digits = read_file(file_name);
+    for ( size_t i=0; i<ROW; ++i ) {
+        for ( size_t j=0; j<COL; ++j ) {
+            board[i][j] = digits[(i * ROW) + j];
+        }
+    }
+}
+
 void print_board(const int board[][COL]) {
-    for ( int i=0; i<ROW; ++i ) {
+    for ( size_t i=0; i<ROW; ++i ) {
         if ( i % 3 == 0 ) {
-            for ( int k=0; k<ROW + 3; ++k ) {
+            for ( size_t k=0; k<ROW + 4; ++k ) {
                 printf("- ");
             }
             printf("\n");
         }
-        for ( int j=0; j<COL; ++j ) {
+        for ( size_t j=0; j<COL; ++j ) {
             if ( j % 3 == 0 ) {
                 printf("| ");
             }
             printf("%d ", board[i][j]);
         }
-    printf("\n");
+    printf("| \n");
     }
 }
 
 // checks if the given number is on the given row
 bool check_row(const int board[][COL], int row, int number) {
-    for ( int j=0; j<COL; ++j ) {
+    for ( size_t j=0; j<COL; ++j ) {
         if ( board[row][j] == number ) 
             return false;
     }
@@ -36,7 +73,7 @@ bool check_row(const int board[][COL], int row, int number) {
 
 // checks if the given number is on the given col
 bool check_col(const int board[][COL], int col, int number) {
-    for ( int i=0; i<ROW; ++i ) {
+    for ( size_t i=0; i<ROW; ++i ) {
         if ( board[i][col] == number )
             return false;
     }
@@ -45,11 +82,11 @@ bool check_col(const int board[][COL], int col, int number) {
 
 // checks if the given number is on the given grid
 bool check_square(const int board[][COL], int row, int col, int number) {
-    int start_row = row - (row % 3);
-    int start_col = col - (col % 3);
+    size_t start_row = row - (row % 3);
+    size_t start_col = col - (col % 3);
 
-    for ( int i=start_row; i<start_row + 3; ++i ) {
-        for ( int j=start_col; j<start_col + 3; ++j ) {
+    for ( size_t i=start_row; i<start_row + 3; ++i ) {
+        for ( size_t j=start_col; j<start_col + 3; ++j ) {
             if ( board[i][j] == number )
                 return false;
         }
@@ -70,8 +107,8 @@ bool is_available(const int board[][COL], int row, int col, int number) {
 }
 
 bool is_board_filled(const int board[][COL]) {
-    for ( int i=0; i<ROW; ++i ) {
-        for ( int j=0; j<COL; ++j ) {
+    for ( size_t i=0; i<ROW; ++i ) {
+        for ( size_t j=0; j<COL; ++j ) {
             if ( board[i][j] == EMPTY )
                 return false;
         }
@@ -84,13 +121,13 @@ bool solve_sudoku(int board[][COL]) {
     if ( is_board_filled(board) )
         return true;
 
-    for ( int i=0; i<9; ++i ) { // go through every row
-        for ( int j=0; j<9; ++j ) { // go through every col
+    for ( size_t i=0; i<9; ++i ) { // go through every row
+        for ( size_t j=0; j<9; ++j ) { // go through every col
             if ( is_empty(board, i, j) ) {
-                for ( int move=1; move<=9; ++move ) { // go through each number
+                for ( size_t move=1; move<=9; ++move ) { // go through each number
                     if ( is_available(board, i, j, move) ) { // if we can make the move
                         board[i][j] = move; // make the move
-                        if ( solve_sudoku(board) ) // recurse
+                        if ( solve_sudoku(board) ) // recursion step
                             return true;
                         board[i][j] = EMPTY; // take the above move back
                     }
@@ -101,21 +138,45 @@ bool solve_sudoku(int board[][COL]) {
     }
 }
 
-int main(int argc, char** argv) {
-    int board[ROW][COL] = {
-        { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-        { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-        { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-        { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-        { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-        { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-        { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-        { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
-        { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
-    };
+void print_usage(void) {
+    printf("%s\n", "Usage: ./main <file_name>");
+}
+
+char* arguments(int argc, char** argv) {
+    if ( argc == 1 ) {
+        fprintf(stdout, "Please provide a file that contains sudoku board.\n");
+        print_usage();
+        exit(EXIT_FAILURE);
+    }
+    else if ( argc == 2 ) {
+        return argv[1];
+    }
+    else {
+        fprintf(stdout, "You have provided too many arguments.\n");
+        print_usage();
+        exit(EXIT_FAILURE);
+    }
+}
+
+void sudoku(int board[][COL], const char* file_name) {
+    init_board(board, file_name);
+    printf("%s", "\nBoard, before solution:\n\n");
+    print_board(board);
+
 
     solve_sudoku(board);
+    printf("%s", "\nBoard, after solution:\n\n");
     print_board(board);
+}
+
+int main(int argc, char** argv) {
+    int board[ROW][COL];
+    const char* file_name;
+
+    file_name = arguments(argc, argv);
+
+    sudoku(board, file_name);
+
     
     return EXIT_SUCCESS;
 }
